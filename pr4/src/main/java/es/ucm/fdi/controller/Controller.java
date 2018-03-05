6807125b4afb816c2923fd12d40031256de6eeb6
b0r1s98 <boris@Boris-Ubuntu>
2018-03-05 20:12:21 +0100

    Cree excepcion SimulatorError Estructura de Traffic Simulator

--- pr4/src/main/java/es/ucm/fdi/controller/Controller.java
+++ pr4/src/main/java/es/ucm/fdi/controller/Controller.java
@@ -1,11 +1,5 @@
 package es.ucm.fdi.controller;
 
 public class Controller {
-	private int time = 0;
-	private RoadMap plano;
-
-	
-	
-	
 	
 }
--- pr4/src/main/java/es/ucm/fdi/controller/TrafficSimulator.java
+++ pr4/src/main/java/es/ucm/fdi/controller/TrafficSimulator.java
@@ -1,5 +1,34 @@
 package es.ucm.fdi.controller;
 
+import java.io.OutputStream;
+import java.util.*;
+
+import es.ucm.fdi.events.Event;
+import es.ucm.fdi.util.MultiTreeMap;
+
 public class TrafficSimulator {
+	private RoadMap objects;
+	private MultiTreeMap<Integer,ArrayList<Event>> events;
+	private int clock;
+	private OutputStream out;
+	
+	public TrafficSimulator(OutputStream out) {
+		this.objects = new RoadMap();
+		this.events = new MultiTreeMap<>();
+		this.clock = 0;
+		this.out = out;
+	}
+	
+	public void addEvent(Event e){
+		
+	}
+	
+	public void run(){
+		
+	}
+	
+	public void reset(){
+		
+	}
 	
 }
