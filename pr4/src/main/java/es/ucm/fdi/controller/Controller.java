d03802e510d68f2952f835e173f32a4e691719c8
b0r1s98 <boris@Boris-Ubuntu>
2018-03-05 19:22:58 +0100

    pom arreglado añadiendo cosas a RoadMap :) Nuevo Traffic Simulator!

--- pr4/src/main/java/es/ucm/fdi/controller/Controller.java
+++ pr4/src/main/java/es/ucm/fdi/controller/Controller.java
@@ -1,5 +1,11 @@
 package es.ucm.fdi.controller;
 
 public class Controller {
+	private int time = 0;
+	private RoadMap plano;
 
+	
+	
+	
+	
 }
--- pr4/src/main/java/es/ucm/fdi/controller/RoadMap.java
+++ pr4/src/main/java/es/ucm/fdi/controller/RoadMap.java
@@ -5,55 +5,57 @@ import java.util.*;
 import es.ucm.fdi.simobject.*;
 
 public class RoadMap {
-	// segun el profe es esta clase quien muve el coche a la siguiente road
-	// la profe tiene bastantes cosas diferentes a como lo dice el nuestro.
-	ArrayList<Vehicle> vehicles; // ArrayList lo mejor en este  caso no?
+	ArrayList<Vehicle> vehicles;
 	ArrayList<Road> roads;
 	ArrayList<Junction> junctions;
-	
-	
-	public Vehicle getVehicle(String ide){
-		for(Vehicle v:vehicles){
-			if(v.getId().equals(ide))return v;
-			
+
+	public Vehicle getVehicle(String ide) {
+		for (Vehicle v : vehicles) {
+			if (v.getId().equals(ide))
+				return v;
 		}
 		return null;
 	}
-	public Road getRoad(String ide){
-		for(Road r:roads){
-		if(r.getId().equals(ide))return r;	
-			
+
+	public Road getRoad(String ide) {
+		for (Road r : roads) {
+			if (r.getId().equals(ide))
+				return r;
 		}
 		return null;
 	}
-	public Junction getJunction(String ide){
-		for(Junction j:junctions){
-		if(j.getId().equals(ide))return j;	
-			
+
+	public Junction getJunction(String ide) {
+		for (Junction j : junctions) {
+			if (j.getId().equals(ide))
+				return j;
 		}
 		return null;
 	}
-	public List<Vehicle> getVehicles(){
-		return vehicles; // no se si esto es válido porque alomejor nos pueden cambiar la lista y no queremos eso
-			
+
+	public List<Vehicle> getVehicles() {
+		return vehicles;
 	}
-	public List<Road> getRoads(){
+
+	public List<Road> getRoads() {
 		return roads;
 	}
-	public List<Junction> getJunctions(){
+
+	public List<Junction> getJunctions() {
 		return junctions;
 	}
-	public void addVehicle(Vehicle v){
+
+	public void addVehicle(Vehicle v) {
 		vehicles.add(v);
-		
 	}
-	public void addRoad(Road r){
+
+	public void addRoad(Road r) {
 		roads.add(r);
-		
 	}
-	public void addJunction(Junction j){
+
+	public void addJunction(Junction j) {
 		junctions.add(j);
-		
 	}
-	// La profe tambien tiene puesto un metodo generateReport y otro de clear, no se si hacen falta.
+	// La profe tambien tiene puesto un metodo generateReport y otro de clear,
+	// no se si hacen falta.
 }
