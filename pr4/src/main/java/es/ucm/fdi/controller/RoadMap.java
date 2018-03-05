7e8fe9ba8d213d6000844722fd57a1a281c70563
b0r1s98 <boris@Boris-Ubuntu>
2018-03-05 19:48:18 +0100

    RoadMap listo (lo que falta lo tengo anotado para ver si lo necesitamos)

--- pr4/src/main/java/es/ucm/fdi/controller/RoadMap.java
+++ pr4/src/main/java/es/ucm/fdi/controller/RoadMap.java
@@ -5,10 +5,43 @@ import java.util.*;
 import es.ucm.fdi.simobject.*;
 
 public class RoadMap {
-	ArrayList<Vehicle> vehicles;
-	ArrayList<Road> roads;
-	ArrayList<Junction> junctions;
+	private List<Vehicle> vehicles;
+	private List<Road> roads;
+	private List<Junction> junctions;
+	
+	private Map<String, Vehicle> mapVehicles;
+	private Map<String, Road> mapRoads;
+	private Map<String, Junction> mapJunctions;
+	
+	public RoadMap() {
+		this.vehicles = new ArrayList<>();
+		this.roads = new ArrayList<>();
+		this.junctions = new ArrayList<>();
+		this.mapVehicles = new HashMap<>();
+		this.mapRoads = new HashMap<>();
+		this.mapJunctions = new HashMap<>();
+	}
+	
+	public Vehicle getVehicle(String id) {return mapVehicles.get(id);}
+	
+	public Road getRoad(String id) {return mapRoads.get(id);}
+	
+	public Junction getJunction(String id) {return mapJunctions.get(id);}
 
+	public List<Vehicle> getVehicles() {return vehicles;}
+
+	public List<Road> getRoads() {return roads;}
+
+	public List<Junction> getJunctions() {return junctions;}
+
+	public void addVehicle(Vehicle v) {vehicles.add(v);}
+
+	public void addRoad(Road r) {roads.add(r);}
+
+	public void addJunction(Junction j) {junctions.add(j);}
+	
+	
+	/*
 	public Vehicle getVehicle(String ide) {
 		for (Vehicle v : vehicles) {
 			if (v.getId().equals(ide))
@@ -32,30 +65,5 @@ public class RoadMap {
 		}
 		return null;
 	}
-
-	public List<Vehicle> getVehicles() {
-		return vehicles;
-	}
-
-	public List<Road> getRoads() {
-		return roads;
-	}
-
-	public List<Junction> getJunctions() {
-		return junctions;
-	}
-
-	public void addVehicle(Vehicle v) {
-		vehicles.add(v);
-	}
-
-	public void addRoad(Road r) {
-		roads.add(r);
-	}
-
-	public void addJunction(Junction j) {
-		junctions.add(j);
-	}
-	// La profe tambien tiene puesto un metodo generateReport y otro de clear,
-	// no se si hacen falta.
+	*/
 }
