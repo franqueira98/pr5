d627ef51f08cda11dad141038e32077dd5bd9ba6
b0r1s98 <boris@Boris-Ubuntu>
2018-03-06 17:38:09 +0100

    Quite EventBuilderParser

--- pr4/src/main/java/es/ucm/fdi/controller/Controller.java
+++ pr4/src/main/java/es/ucm/fdi/controller/Controller.java
@@ -5,7 +5,11 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import es.ucm.fdi.eventbuilders.EventBuilderParser;
+import es.ucm.fdi.eventbuilders.EventBuilder;
+import es.ucm.fdi.eventbuilders.JunctionEventBuilder;
+import es.ucm.fdi.eventbuilders.MakeVehicleFaultyEventBuilder;
+import es.ucm.fdi.eventbuilders.RoadEventBuilder;
+import es.ucm.fdi.eventbuilders.VehicleEventBuilder;
 import es.ucm.fdi.events.Event;
 import es.ucm.fdi.ini.Ini;
 import es.ucm.fdi.ini.IniSection;
@@ -15,7 +19,7 @@ public class Controller {
 	private InputStream in;
 	private OutputStream out;
 	private int ticks;
-	
+
 	public Controller(TrafficSimulator simulation, InputStream in,
 			OutputStream out, int ticks) {
 		this.simulation = simulation;
@@ -23,22 +27,34 @@ public class Controller {
 		this.out = out;
 		this.ticks = ticks;
 	}
-	
-	public void run() throws IOException{
+
+	public void run() throws IOException {
 		loadEvents();
-		//cargar elementos de eventos de este tiempo
+		// cargar elementos de eventos de este tiempo
 	}
-	
-	public void reset(){
-		//No se si sirve de algo
+
+	public void reset() {
+		// No se si sirve de algo
 	}
-	
+
 	public void loadEvents() throws IOException {
 		Ini init = new Ini(in);
 		List<IniSection> list = init.getSections();
-		for(IniSection i : list){
-			Event event = EventBuilderParser.parseBuilder(i).parse(i);
-			simulation.addEvent(event);
+
+		EventBuilder[] avaliableEvents = { new VehicleEventBuilder(),
+				new RoadEventBuilder(), new JunctionEventBuilder(),
+				new MakeVehicleFaultyEventBuilder() };
+
+		for (IniSection i : list) {
+			boolean found = false;
+			for (EventBuilder eventB : avaliableEvents) {
+				if (i.getTag().equals(eventB.getTitle())) {
+					simulation.addEvent(eventB.parse(i));
+					found = true;
+				}
+			}
+
+			if(!found) throw new IllegalArgumentException();
 		}
 	}
 	
