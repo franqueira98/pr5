a7f931d922201f2797fcb6659189e2cfefd9da07
franqui <migfranq@ucm.es>
2018-03-09 22:27:42 +0100

    Creacion newVehicleEvent

---
+++ pr4/src/main/java/es/ucm/fdi/events/newJunctionEvent.java
@@ -0,0 +1,18 @@
+package es.ucm.fdi.events;
+
+import es.ucm.fdi.controller.RoadMap;
+
+public class newJunctionEvent extends Event {
+
+	public newJunctionEvent(int time) {
+		super(time);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(RoadMap things) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
