553f9ef043c14d55dadf6d3b304a9201a1b49d81
franqui <migfranq@ucm.es>
2018-03-18 23:21:33 +0100

    Versión de la entrega

--- pr4/src/test/java/es/ucm/fdi/simobject/BikeTest.java
+++ pr4/src/test/java/es/ucm/fdi/simobject/BikeTest.java
@@ -17,14 +17,14 @@ public class BikeTest {
 		itinerary.add(new Junction("j2"));
 		itinerary.add(new Junction("j3"));
 		Bike v = new Bike(10, itinerary, "v1", "bike");
-		Bike v1 = new Bike(15, itinerary, "v2", "bike");
+		Bike v1 = new Bike(30, itinerary, "v2", "bike");
 		v.setVelocidadActual(10);
 		v1.setVelocidadActual(2);
 		v.setTiempoAveria(1);
 		v1.setTiempoAveria(1);
 
-		assertTrue("No pone bien el tiempo de averia",v1.getTiempoAveria()!=0);
-		assertTrue("No pone bien el tiempo de averia", v.getTiempoAveria()==0);
+		assertTrue("No pone bien el tiempo de averia",v1.getTiempoAveria()==0);
+		assertTrue("No pone bien el tiempo de averia", v.getTiempoAveria()!=0);
 	}
 
 }
